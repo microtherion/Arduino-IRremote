04a1723f7aa04c78415ab00552e80304a45c26f2
Matthias Neeracher <neeracher@mac.com>
2013-03-31 01:52:07 +0100

    Remove low speed tweak

--- IRremote.cpp
+++ IRremote.cpp
@@ -280,13 +280,6 @@ void IRsend::space(int time) {
   // Sends an IR space for the specified number of microseconds.
   // A space is no output, so the PWM output is disabled.
   TIMER_DISABLE_PWM; // Disable pin 3 PWM output
-#if F_CPU < 8000000
-  //
-  // At slow clock rates, our bit processing time starts becoming
-  // significant.
-  //
-  time -= 80;
-#endif
   delayMicroseconds(time);
 }
 #endif
